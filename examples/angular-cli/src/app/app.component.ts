9141a9d6fc3df515a89ac01a0ac000f7cf5a1795
elari01 <Rijin.Elayambari@ca.com>
2017-12-15 02:01:02 +0530

    Angular Cli Example fixed

--- examples/angular-cli/src/app/app.component.ts
+++ examples/angular-cli/src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, AfterViewInit, DoCheck} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
   title = 'app';
+  counter = 0;
   options: Object;
-  constructor(){
+
+  constructor() {
     this.options = {
-      title : { text : 'simple chart' },
+      title: {text: 'simple chart'},
       series: [{
         data: [29.9, 71.5, 106.4, 129.2],
       }]
     };
+  }
+
+  ngDoCheck() {
+    this.counter++;
   }
 }
--- examples/angular-cli/src/app/app.module.ts
+++ examples/angular-cli/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ChartModule } from '@rijine/ngx-highcharts';
 
+import * as Highcharts from 'highcharts'
+
 import { AppComponent } from './app.component';
 
 
@@ -11,7 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ChartModule.forRoot(require('highcharts/highstock'))
+    ChartModule.forRoot(Highcharts)
   ],
   providers: [],
   bootstrap: [AppComponent]
